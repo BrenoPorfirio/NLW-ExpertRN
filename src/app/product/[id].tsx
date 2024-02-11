67c7f8a9af238031db7c00f836c12394dc1f8f51
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2024-02-11 12:09:27 -0300

    code adjustments

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/button";
 import { Feather } from "@expo/vector-icons"
 import { LinkButton } from "@/components/link-button";
 import { useCartStore } from "@/stores/cart-store";
+import { Redirect } from "expo-router";
 
 export default function Product() {
     const { id } = useLocalSearchParams()
@@ -14,11 +15,15 @@ export default function Product() {
 
     const navigation = useNavigation()
 
-    const product = PRODUCTS.filter((item) => item.id === id)[0]
+    const product = PRODUCTS.find((item) => item.id === id)
 
     function handleAddToCart() {
-        cartStore.add(product)
+        cartStore.add(product!)
         navigation.goBack()
+    }
+
+    if (!product) {
+        return <Redirect href="/" />
     }
 
     return (
@@ -30,6 +35,8 @@ export default function Product() {
             />
 
             <View className="p-5 mt-8 flex-1">
+                <Text className="text-white text-xl font-heading">{product.title}</Text>
+
                 <Text className="text-lime-400 text-2xl font-heading my-2">
                     {formatCurrency(product.price)}
                 </Text>
